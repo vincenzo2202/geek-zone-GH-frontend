eefef77e8f4fec4a201dd397d97fdcfb37c6c848
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-12 14:35:34 +0100

    added toggle to feedcard component to inclede comments

--- src/common/FeedCard/FeedCard.jsx
+++ src/common/FeedCard/FeedCard.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './FeedCard.css';
 
 export const FeedCard = ({ key, userPhoto, user_id, userName, userLast_name, title, content, photo }) => {
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleCollapse = () => {
+        setCollapsed(!collapsed);
+    };
+
     return (
         <div className='card'>
+
             <div className="feed-card" key={key}>
-                <div className='desc'>Photo: </div> 
+                <div className='desc'>Photo: </div>
                 <img className="pic-avatar" src={userPhoto} alt={userPhoto} />
                 <div className='desc'>User ID: </div>
                 <div className="user-id"> {user_id}</div>
@@ -16,23 +25,32 @@ export const FeedCard = ({ key, userPhoto, user_id, userName, userLast_name, tit
                 <div className="feed-title">{title}</div>
                 <div className='desc'>Content: </div>
                 <div className="feed-content">{content}</div>
-             
-              
                 {
                     photo
                         ? (
                             <>
-                            <div className='desc'>Photo:  </div> 
-                            <img className="pic-feed" src={photo} alt={photo} />
+                                <div className='desc'>Photo:  </div>
+                                <img className="pic-feed" src={photo} alt={photo} />
                             </>
                         )
                         : (
                             <div ></div>
                         )
-
-
                 }
             </div>
+
+
+            <button className="button-spoiler" onClick={toggleCollapse}>
+                    {collapsed ? "comments" : "comments"}
+                </button>
+
+            {!collapsed && (
+                    <div className="comments"> 
+                        <div className="comment">Prueba de comentario</div> 
+                        {/* aqui va el componente de comentarios */}
+                    </div>
+                )}
+
         </div >
     )
 }; 
