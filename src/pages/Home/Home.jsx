cb5618265096095bcc21e48950d929497551ce55
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-20 18:52:16 +0100

    style: added logo to and adjustements to the css files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css';
 
+import '../../assets/logo5.png';
 export const Home = () => {
     return (
         <div className='home-body'>
-           <div className='title-home'>
-            <h1 >Welcome to the GEEK ZONE</h1> 
-            </div> 
+            <div className='title-home'>
+                <div className='logo-geek-zone'><img src="../../src/assets/logo5.png" alt="" />  </div>
+                <div className='title-geek-zone'>Más que una academia,<br/> somos una comunidad</div>
+            </div>
         </div>
     );
 };
- 
+
