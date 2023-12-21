ca9ab2ab88972e9da29092c5e0f00ff559b974bd
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-21 17:26:17 +0100

    feat: added style to user card component

--- src/common/UserCard/UserCard.jsx
+++ src/common/UserCard/UserCard.jsx
@@ -25,8 +25,7 @@ export const CardUser = ({name,last_name, photo, phone_number, email, userid, ci
         <div className="card-worker" userid={userid}  dragable="false" onClick={()=>goTOUserProfile(id)}> 
             <img className="worker" src={photo} alt={photo} /> 
             <div className="desc-user">
-                <div className="name">{name}</div>
-                <div className="last_name">{last_name}</div>
+                <div className="name">{name} {last_name}</div> 
                 <div className="email">{email}</div>
                 <div className="phone">{phone_number}</div>
                 <div className="city">{city}</div>
