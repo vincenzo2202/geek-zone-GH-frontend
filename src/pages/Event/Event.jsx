8b352663aff0672cef800175174b7e3af894206c
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-21 20:21:02 +0100

    style: added lateral banner to event page

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -3,7 +3,7 @@ import './Event.css';
 import { selectToken } from '../userSlice';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getAllEvents  } from '../../services/apiCalls';
+import { getAllEvents } from '../../services/apiCalls';
 import { CreateEventCard } from '../../common/CreateEventCard/CreateEventCard';
 import { EventCard } from '../../common/EventCard/EventCard';
 import { jwtDecode } from 'jwt-decode';
@@ -13,7 +13,7 @@ export const Event = () => {
     const rdxToken = useSelector(selectToken);
     const tokenDecoded = jwtDecode(rdxToken);
     const navigate = useNavigate();
-    const dispatch = useDispatch(); 
+    const dispatch = useDispatch();
     const [event, setEvent] = useState([])
     const [role, setRole] = useState('');
 
@@ -24,7 +24,7 @@ export const Event = () => {
                     response => {
                         setEvent(response.data.data);
                     })
-                .catch(error => console.log(error)); 
+                .catch(error => console.log(error));
         } else {
             navigate("/");
         }
@@ -32,27 +32,35 @@ export const Event = () => {
 
     return (
         <div className="event-body">
-            <div className='create-event-modal'>
-                {
-                    tokenDecoded.role === "admin" || tokenDecoded.role === "super_admin" &&
-                    <CreateEventCard />
-                }
+            <div className='left-banner-event'>
+                <img src="../../src/assets/banner2.gif" alt="event" />
             </div>
-            <div className='line-div'>Here are all the events </div>
-            <div className='event-background'>
-                {
-                    [...event].reverse().map((event, index) => (
-                        <EventCard
-                            key={index}
-                            eventId={event.id}
-                            title={event.title}
-                            content={event.content}
-                            date={event.event_date}
-                            time={event.event_time}
-                            creator={event.user_id}
-                            role={role}
-                        />
-                    ))}
+            <div className='center-event'>
+                <div className='create-event-modal'>
+                    {
+                        tokenDecoded.role === "admin" || tokenDecoded.role === "super_admin" &&
+                        <CreateEventCard />
+                    }
+                </div>
+                <div className='line-div'>Here are all the events </div>
+                <div className='event-background'>
+                    {
+                        [...event].reverse().map((event, index) => (
+                            <EventCard
+                                key={index}
+                                eventId={event.id}
+                                title={event.title}
+                                content={event.content}
+                                date={event.event_date}
+                                time={event.event_time}
+                                creator={event.user_id}
+                                role={role}
+                            />
+                        ))}
+                </div>
+            </div>
+            <div className='right-banner-event'>
+                <img src="../../src/assets/banner2.gif" alt="event" />
             </div>
         </div>
     )
