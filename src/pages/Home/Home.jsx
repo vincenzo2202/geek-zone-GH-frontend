2bb54adfb9bea9d3c12a476537c63cae197851cd
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-22 18:10:06 +0100

    feat: import logo to check the deploy

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.css';
+import logo5 from '../../assets/logo5.png';
 
 import '../../assets/logo5.png';
 export const Home = () => {
     return (
         <div className='home-body'>
             <div className='title-home'>
-                <div className='logo-geek-zone-home'><img className='logo-geek-zone-home' src="../../src/assets/logo5.png" alt="logo-geek-zone-home" />  </div>
+                <div className='logo-geek-zone-home'><img className='logo-geek-zone-home' src= {logo5} alt="logo-geek-zone-home" />  </div>
                 <div className='title-geek-zone'>Más que una academia,<br/> somos una comunidad</div>
             </div>
         </div>
